e178080cecd5e62ba90d0fe0086eda2b74ffbe49
Keiran Jensen <keiranjensen@gmail.com>
2026-01-15 21:43:52 -0500

    tutorial part 5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,6 +168,8 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.parser.CamelCaseMultiPartParser",
         "djangorestframework_camel_case.parser.CamelCaseJSONParser",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 CACHES = {
