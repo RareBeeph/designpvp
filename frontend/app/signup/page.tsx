93e7252745861fe79e3a232fac4885ab52669442
Keiran Jensen <keiranjensen@gmail.com>
2026-02-09 21:56:13 -0500

    start work on signup page

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,15 +1,18 @@
-import { usePostAuthLogin } from '@/api/allauth';
+import { usePostAuthSignup } from '@/api/allauth';
 import { Container, Stack } from '@mui/material';
 
 import LoginForm from '@/components/LoginForm';
 
 export default function Home() {
-  const mutation = usePostAuthLogin();
+  const mutation = usePostAuthSignup();
+  // TODO: implement these
+  // const onSuccess = async () => {};
+  // const onError = async () => {};
 
   return (
     <Stack sx={{ alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%' }}>
       <Container sx={{ width: '40%', minWidth: 'min-content' }} maxWidth="sm">
-        <LoginForm mutation={mutation} />
+        <LoginForm {...{ mutation }} />
       </Container>
     </Stack>
   );
--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-import { usePostAuthLogin } from '@/api/allauth';
+import { usePostAuthLogin, usePostAuthSignup } from '@/api/allauth';
 import {
   Button,
   Paper,
@@ -13,10 +13,15 @@ import {
   useTheme,
 } from '@mui/material';
 
-export default function LoginForm() {
+type MutationType = ReturnType<typeof usePostAuthLogin> | ReturnType<typeof usePostAuthSignup>;
+
+export default function LoginForm(props: {
+  mutation: MutationType;
+  onSuccess?: () => Promise<void>;
+  onError?: () => Promise<void>;
+}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const login = usePostAuthLogin();
 
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down('md'));
@@ -25,6 +30,12 @@ export default function LoginForm() {
   const headerVariant = isXL ? 'h4' : isSmall ? 'h6' : 'h5';
   const textFieldSize = isSmall ? 'small' : 'medium';
   const buttonSize = isSmall ? 'medium' : 'large';
+
+  const onClick = async () =>
+    props.mutation.mutate(
+      { data: { username, password } },
+      { onSuccess: props.onSuccess, onError: props.onError },
+    );
 
   return (
     <Paper sx={{ p: { xs: 2, md: 2, xl: 2.5 }, minWidth: 'max-content' }}>
@@ -44,11 +55,7 @@ export default function LoginForm() {
           size={textFieldSize}
           onChange={e => setPassword(e.target.value)}
         />
-        <Button
-          variant="contained"
-          size={buttonSize}
-          onClick={() => login.mutate({ data: { username, password } })}
-        >
+        <Button variant="contained" size={buttonSize} onClick={onClick}>
           Submit
         </Button>
       </Stack>
