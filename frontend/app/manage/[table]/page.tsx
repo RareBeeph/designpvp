93f35b8613865f737d89ab3c525bbe04e76c414a
Keiran Jensen <keiranjensen@gmail.com>
2026-03-20 21:35:45 -0400

    Lift team/event table data logic to page level

--- frontend/app/manage/[table]/page.tsx
+++ frontend/app/manage/[table]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useState } from 'react';
+import { JSX, useState } from 'react';
 
+import { useEventsList, useTeamsList } from '@/api/backend';
 import AddIcon from '@mui/icons-material/Add';
 import { Button, Container, Fab, Modal, Stack } from '@mui/material';
 import { useParams } from 'next/navigation';
+import { pascalCase } from 'text-case';
 
 import TeamManagerForm from '@/components/TeamManagerForm';
 import DataTable from '@/components/api/DataTable';
@@ -12,10 +14,49 @@ import Padding from '@/components/form/Padding';
 
 import { useBreakpoint } from '@/hooks/useBreakpoint';
 
+const eventsColumns = [
+  { accessorKey: 'id', header: 'ID', size: 0, grow: true },
+  { accessorKey: 'name', header: 'Name', size: 0, grow: true },
+  { accessorKey: 'starts', header: 'Starts', size: 0, grow: true },
+  { accessorKey: 'ends', header: 'Ends', size: 0, grow: true },
+];
+
+const teamsColumns = [
+  { accessorKey: 'id', header: 'ID', size: 0, grow: true },
+  { accessorKey: 'name', header: 'Name', size: 0, grow: true },
+  { accessorKey: 'event', header: 'Event', size: 0, grow: true },
+];
+
 export default function Teams() {
   const breakpoint = useBreakpoint();
   const [open, setOpen] = useState(false);
   const { table }: { table: 'events' | 'teams' } = useParams();
+  const eventsList = useEventsList();
+  const teamsList = useTeamsList();
+
+  let dataTable: JSX.Element;
+  switch (table) {
+    case 'teams':
+      dataTable = (
+        <DataTable
+          data={
+            teamsList.data
+              ?.map(({ id, name, event: eventId }) => {
+                const event = (eventsList.data ?? []).find(event => event.id === eventId);
+                return { id, name, event: event?.name ?? '' };
+              })
+              .toSorted(({ id: idA }, { id: idB }) => idA - idB) ?? []
+          }
+          columns={teamsColumns}
+          sx={{ flex: 4 }}
+        />
+      );
+      break;
+    case 'events':
+      dataTable = (
+        <DataTable data={eventsList.data ?? []} columns={eventsColumns} sx={{ flex: 4 }} />
+      );
+  }
 
   return (
     <Container disableGutters>
@@ -43,7 +84,7 @@ export default function Teams() {
                 setOpen(!open);
               }}
             >
-              Add New Team
+              Add New {pascalCase(table.slice(0, -1))}
             </Button>
           : <Fab
               color="primary"
@@ -60,7 +101,7 @@ export default function Teams() {
         </Stack>
         <Stack direction={'row'}>
           <Padding flex={1} />
-          <DataTable table={table} sx={{ flex: 4 }} />
+          {dataTable}
           <Padding flex={1} />
         </Stack>
       </Stack>
--- frontend/components/api/DataTable.tsx
+++ frontend/components/api/DataTable.tsx
@@ -1,35 +1,21 @@
 'use client';
 
-import { MaterialReactTable } from 'material-react-table';
-import { JSX } from 'react';
+import { MRT_RowData, MaterialReactTable } from 'material-react-table';
 
-import { useEventsList, useTeamsList } from '@/api/backend';
 import { PaperProps } from '@mui/material';
 import { usePathname, useRouter } from 'next/navigation';
 
 import { useBreakpoint } from '@/hooks/useBreakpoint';
 
-const eventsColumns = [
-  { accessorKey: 'id', header: 'ID', size: 0, grow: true },
-  { accessorKey: 'name', header: 'Name', size: 0, grow: true },
-  { accessorKey: 'starts', header: 'Starts', size: 0, grow: true },
-  { accessorKey: 'ends', header: 'Ends', size: 0, grow: true },
-];
-
-const teamsColumns = [
-  { accessorKey: 'id', header: 'ID', size: 0, grow: true },
-  { accessorKey: 'name', header: 'Name', size: 0, grow: true },
-  { accessorKey: 'event', header: 'Event', size: 0, grow: true },
-];
-
 export default function DataTable({
-  table,
+  data,
+  columns,
   children: _children,
   ...props
-}: PaperProps & { table: 'teams' | 'events' }) {
-  const teamsList = useTeamsList();
-  const eventsList = useEventsList();
-
+}: PaperProps & {
+  data: MRT_RowData[];
+  columns: { accessorKey: string; header: string; size: number; grow: boolean }[];
+}) {
   const breakpoint = useBreakpoint();
   const router = useRouter();
   const pathName = usePathname();
@@ -51,48 +37,16 @@ export default function DataTable({
     },
   };
 
-  let tableComponent: JSX.Element;
-  switch (table) {
-    case 'teams':
-      const teamsData =
-        teamsList.data
-          ?.map(({ id, name, event: eventId }) => {
-            const event = eventsList.data?.find(event => event.id === eventId);
-            return { id, name, event: event?.name ?? '' };
-          })
-          .toSorted(({ id: idA }, { id: idB }) => idA - idB) ?? [];
-      tableComponent = (
-        <MaterialReactTable
-          columns={teamsColumns}
-          data={teamsData}
-          muiTableBodyCellProps={({ row }) => ({
-            onClick: () => {
-              router.push(pathName + '/' + row.getValue('id'));
-            },
-          })}
-          {...tableProps}
-        />
-      );
-      break;
-    case 'events':
-      const eventsData = eventsList.data ?? [];
-      tableComponent = (
-        <MaterialReactTable
-          columns={eventsColumns}
-          data={eventsData}
-          muiTableBodyCellProps={({ row }) => ({
-            onClick: () => {
-              router.push(pathName + '/' + row.getValue('id'));
-            },
-          })}
-          {...tableProps}
-        />
-      );
-      break;
-    default:
-      // probably should like. 404 or something idk
-      tableComponent = <></>;
-  }
-
-  return tableComponent;
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      muiTableBodyCellProps={({ row }) => ({
+        onClick: () => {
+          router.push(pathName + '/' + row.getValue('id'));
+        },
+      })}
+      {...tableProps}
+    />
+  );
 }
